8beb09c02bc285d2bf5bb1308fb842edc114f573
Martyna <soroka@uxf.cz>
2023-12-27 22:58:04 +0100

    V 1.1 27.12

---
+++ script.js
@@ -0,0 +1,23 @@
+
+/*
+
+var x = document.getElementById("myAudio");
+
+const play = document.querySelector("#play")
+
+play/addEventListener("click", e => {
+    x.play()
+}
+)
+
+
+/*
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
+
+*/
