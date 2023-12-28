f03d4f77a0165d269dba980a94f65c752a85ccd0
Martyna <soroka@uxf.cz>
2023-12-28 15:48:36 +0100

    V 2.0 28.12

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
+const btn = document.getElementById("btn")
 
-/*
-
-var x = document.getElementById("myAudio");
-
-const play = document.querySelector("#play")
-
-play/addEventListener("click", e => {
-    x.play()
-}
-)
-
-
-/*
-function playAudio() {
-  x.play();
-}
-
-function pauseAudio() {
-  x.pause();
-}
-
-*/
+btn.addEventListener('click', function changeColor() {
+  document.body.classList.toggle("green")
+})
